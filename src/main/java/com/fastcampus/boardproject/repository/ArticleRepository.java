1318f1886e1db19008658d50262bceabcac6b126
hyojunnii <96437859+hyojunnii@users.noreply.github.com>
2023-04-26 23:02:14 +0900

    #8 - Querydsl 지원을 이용하여 api 검색 옵션 추가
    `QuerydslPredicateExecutor`, `QuerydslBinderCustomizer`를 이용하여 간단하게 개발

--- src/main/java/com/fastcampus/boardproject/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/boardproject/repository/ArticleRepository.java
@@ -1,9 +1,31 @@
 package com.fastcampus.boardproject.repository;
 
 import com.fastcampus.boardproject.domain.Article;
+import com.fastcampus.boardproject.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>, // 검색기능
+        QuerydslBinderCustomizer<QArticle> // Entity // 검색 customize
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) { // default 로 인터페이스에 기능 구현
+        // 필드 필터링
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+        // 부분 검색 // (StringExpression::likeIgnoreCase); like '${v}',  % 수동으로 붙임
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // like '%${v}%'
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq); // 시분초까지 동일해야함
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
