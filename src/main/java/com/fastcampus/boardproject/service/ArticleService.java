a014eed60b802369501b996809a5e18e57826d41
hyojunnii <96437859+hyojunnii@users.noreply.github.com>
2023-05-02 19:28:39 +0900

    #42 - 게시판, 게시글 뷰의 기능에 대응하는 자바 코드 구현

--- src/main/java/com/fastcampus/boardproject/service/ArticleService.java
+++ src/main/java/com/fastcampus/boardproject/service/ArticleService.java
@@ -1,11 +1,12 @@
 package com.fastcampus.boardproject.service;
 
 import com.fastcampus.boardproject.domain.Article;
-import com.fastcampus.boardproject.domain.type.SearchType;
+import com.fastcampus.boardproject.domain.UserAccount;
+import com.fastcampus.boardproject.domain.constant.SearchType;
 import com.fastcampus.boardproject.dto.ArticleDto;
 import com.fastcampus.boardproject.dto.ArticleWithCommentsDto;
 import com.fastcampus.boardproject.repository.ArticleRepository;
-
+import com.fastcampus.boardproject.repository.UserAccountRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,6 +24,7 @@ import java.util.List;
 public class ArticleService {
 
     private final ArticleRepository articleRepository;
+    private final UserAccountRepository userAccountRepository;
 
     @Transactional(readOnly = true) // 읽는 작업만
     public Page<ArticleDto> searchArticles(SearchType searchType, String searchKeyword, Pageable pageable) {
@@ -40,21 +42,29 @@ public class ArticleService {
     }
 
     @Transactional(readOnly = true)
-    public ArticleWithCommentsDto getArticle(Long articleId) {
+    public ArticleWithCommentsDto getArticleWithComments(Long articleId) {
         return articleRepository.findById(articleId)
                 .map(ArticleWithCommentsDto::from)
                 .orElseThrow(() -> new EntityNotFoundException("게시글이 없습니다 - articleId: " + articleId));
     }
 
-    public void saveArticle(ArticleDto dto) {
-        articleRepository.save(dto.toEntity());
+    @Transactional(readOnly = true)
+    public ArticleDto getArticle(Long articleId) {
+        return articleRepository.findById(articleId)
+                .map(ArticleDto::from)
+                .orElseThrow(() -> new EntityNotFoundException("게시글이 없습니다 - articleId: " + articleId));
     }
 
-    public void updateArticle(ArticleDto dto) {
+    public void saveArticle(ArticleDto dto) {
+        UserAccount userAccount = userAccountRepository.getReferenceById(dto.userAccountDto().userId());
+        articleRepository.save(dto.toEntity(userAccount));
+    }
+
+    public void updateArticle(Long articleId, ArticleDto dto) {
         try {
-            Article article = articleRepository.getReferenceById(dto.id());
-            if(dto.title() != null) {article.setTitle(dto.title());}
-            if(dto.content() != null) {article.setContent(dto.content());}
+            Article article = articleRepository.getReferenceById(articleId);
+            if (dto.title() != null) {article.setTitle(dto.title());}
+            if (dto.content() != null) {article.setContent(dto.content());}
             article.setHashtag(dto.hashtag());
             // Transactional 속성때문에 따로 save 안해도 각각 쿼리 날라감
         } catch (EntityNotFoundException e) {
@@ -72,7 +82,7 @@ public class ArticleService {
 
     @Transactional(readOnly = true)
     public Page<ArticleDto> searchArticlesViaHashtag(String hashtag, Pageable pageable) {
-        if(hashtag == null || hashtag.isBlank()) {
+        if (hashtag == null || hashtag.isBlank()) {
             return Page.empty(pageable);
         }
         return articleRepository.findByHashtag(hashtag, pageable).map(ArticleDto::from);
@@ -81,5 +91,5 @@ public class ArticleService {
     public List<String> getHashtags() {
         return articleRepository.findAllDistinctHashtags();
     }
-    
+
 }
